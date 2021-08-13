4096c313516d9921a269ed4325cebb6b576650e8
Humberto Ramone <humbertorody@gmail.com>
2021-08-13 13:16:48 -0300

    -Comentario na Classe conexão

--- src/dao/Conexao.java
+++ src/dao/Conexao.java
@@ -5,10 +5,12 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexao {
+    //Atributos da classe Conexa
     private static String connectionString = "jdbc:mysql://localhost:3306";
     private static String user = "root";
     private static String pass = "@Abc123";
 
+    //Funcao que abre a conexão com o banco dados
     public static Connection abreConexao() {
         try {
             return DriverManager.getConnection(connectionString, user, pass);
